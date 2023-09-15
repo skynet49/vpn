b490e3c41edd0da3ffc4c44266813dd8f76b0da5
skynet49 <142214516+skynet49@users.noreply.github.com>
2023-09-15 07:12:26 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"Version":"2.1","Url":"https://github.com/skynet49/vpn/raw/main/TRUE.ZSERVER.zip","Text":"อัพเดท 14 กันยายน 2566"}
+{"Version":"2.2","Url":"https://github.com/skynet49/vpn/raw/main/TRUE.ZSERVER.zip","Text":"อัพเดท 15 กันยายน 2566"}
