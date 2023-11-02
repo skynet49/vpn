79b8f5195a230e7a922e06855faa4c29f33bb24c
skynet49 <142214516+skynet49@users.noreply.github.com>
2023-11-02 13:17:08 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,1 +1,1 @@
-{"Version":"2.6","Url":"https://github.com/skynet49/vpn/raw/main/allserver.zip","Text":"อัพเดท 2 พฤศจิกายน 2566"}
+{"Version":"2.7","Url":"https://github.com/skynet49/vpn/raw/main/allserver.zip","Text":"อัพเดท 2 พฤศจิกายน 2566"}
